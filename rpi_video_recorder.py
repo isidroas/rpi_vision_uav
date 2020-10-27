d9df06341b7deba13bc35b85801fd1d0938bd2eb
Isidro <isidroariass@hotmail.es>
2020-10-27 19:27:53 +0100

    Custom camera parameters

---
+++ rpi_video_recorder.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import os
+from time import sleep
+
+# Configure camera
+cmd = "v4l2-ctl -c exposure_time_absolute=50 -c auto_exposure=1"
+os.system(cmd)
+
+
+# Record Video
+print(datetime.now())
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+output_file=timestamp+".h264"
+
+cmd = "ffmpeg -f v4l2 -framerate 30 -video_size 640x480 -i /dev/video0 " + output_file
+os.system(cmd)
+
+sleep(3)
+
+# todo convert to mp4
+
+
+print(timestamp)
