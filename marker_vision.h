782d26c8854ade2d738f3c5205c7902044c0a982
Isidro Arias <isidroariass@hotmail.es>
2020-11-06 20:41:08 +0100

    Mejor deducción de la inversión de la postura

--- marker_vision.h
+++ marker_vision.h
@@ -283,44 +283,43 @@ bool VisionClass::detect_marker(Eigen::Vector3d &pos, Eigen::Vector3d &eul){
 }
 
 void VisionClass::InvertPose(Eigen::Vector3d &pos, Eigen::Vector3d &eul, Vec3d &rvec, Vec3d &tvec){
-    // tvecs and rvecs are translation and rotation of the marker with respect camera axis
-    // camera position coordinates are the same as autopilot
+    /* @brief Invierte la posición y la rotación. También corrige la posición de la cámara con respecto al UAV
+     * @param pos   posición del uav/cámara con respecto al marcador
+     * @param eul   orientación del uav con respecto al marcador. El orden de los elementos son 0: roll, 1: pitch, 2: yaw
+     * @param rvec  Vector de rotación del marcador con respecto a los ejes de la cámara
+     * @param tvec  Vector de translación del marcador con respecto a los ejes de la cámara
+     */
+
+    Eigen::Vector3d     pos_marker_in_camera(tvec[0],tvec[1],tvec[2]);
     
-    // rvecs are rotation_vectors. Here it is transformed to rotation matrix
-    cv::Mat  rot_mat;
+    // Transformación de vector de rotación a matriz de rotación
+    cv::Mat                     rot_mat;
+    Eigen::Matrix3d             rot_mat_marker_from_camera; 
     Rodrigues(rvec,rot_mat);
+    cv::cv2eigen(rot_mat, rot_mat_marker_from_camera);
+
+    // La inversa de una matriz de rotación es igual a su traspuesta
+    Eigen::Matrix3d     rot_mat_camera_from_marker = rot_mat_marker_from_camera.transpose() ; 
+
+    // Se obtiene la posición del marcador en unos ejes paralelos al marcador centrados en la cámara
+    Eigen::Vector3d     pos_marker_in_marker_axis = rot_mat_camera_from_marker*pos_marker_in_camera;
+
+    // Si queremos que la posición esté centrada en el marcador y no en la cámara, es necesario negarla
+    pos = -pos_marker_in_marker_axis;
     
-    Eigen::Matrix3d rot_mat_eig; 
-    cv::cv2eigen(rot_mat,rot_mat_eig);
-    
-    
-    // rotate 180 degrees in x axis in order to get z point down. Also rotate 180º in z axis
-    // Rx=[[1, 0, 0], [0, -1, 0], [0, 0, -1]]
-    // Rz=[[-1, 0, 0], [0, -1, 0], [0, 0, 1]]
-    Eigen::Matrix3d  xz_rotation;
-    xz_rotation << -1,  0,  0,
-                    0,  1,  0,
-                    0,  0, -1;
-    Eigen::Matrix3d  x_rotation;
-    x_rotation << 1,   0,   0,
-                  0,  -1,   0,
-                  0,   0,  -1;
-    Eigen::Matrix3d  z_rotation;
-    z_rotation <<-1,   0,   0,
-                  0,  -1,   0,
-                  0,   0,   1;
-    //Eigen::Matrix3d rot_mat_aux=x_rotation*rot_mat_eig.transpose()*z_rotation; 
-    //Eigen::Matrix3d rot_mat_aux=rot_mat_eig.transpose(); 
-    
-    // Get position and rotation of camera in marker axis
-    Eigen::Vector3d t_in(tvec[0],tvec[1],tvec[2]);
-    // TODO: why is neccesary minus sign?
-    //pos = -rot_mat_aux*t_in;
-    //pos = rot_mat_eig.transpose()*t_in;
-    pos = t_in;
-    
-    //eul = rotationMatrixToEulerAngles(rot_mat_aux);
-    eul = rotationMatrixToEulerAngles(rot_mat_eig);
+    // Aquí debemos de tener en cuenta la rotación de la cámara con respecto al uav. Esta es de 180º alrededor del eje z
+    // (de la cámara o del uav, da igual, por tanto da igual postmultiplicar que premultiplicar)
+    Eigen::Matrix3d             rot_mat_camera_from_uav;
+    rot_mat_camera_from_uav     << -1,   0,   0,
+                                    0,  -1,   0,
+                                    0,   0,   1;
+    Eigen::Matrix3d             rot_mat_marker_from_uav = rot_mat_marker_from_camera * rot_mat_camera_from_uav; 
+
+    // Se obtiene la orientación del uav visto desde el marcador
+    Eigen::Matrix3d             rot_mat_uav_from_marker = rot_mat_marker_from_uav.transpose() ; 
+
+    // Se obtiene los ángulos de Tait–Bryan en el orden Z-Y-X (ángulos de euler)
+    eul = rotationMatrixToEulerAngles(rot_mat_uav_from_marker);
 }
 
 bool VisionClass::readVisionParameters(string filename) {
